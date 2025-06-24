bc4687aa742d82c5630be986183400f8036f7458
Venícius <miguelsilva8v@gmail.com>
2025-06-24 14:45:07 -0300

    fix(deploy): login limiter

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.set('trust proxy', 1);
 app.use(helmet());
-app.use(mongoSanitize());
+app.use(mongoSanitize({ replaceWith: '_' }));
 
 connectDB();
 connectRedis();
